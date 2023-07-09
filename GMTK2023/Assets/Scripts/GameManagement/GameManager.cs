32bc1618b3914c122d413c2e26c2a006391f26ed
Evan King <evantheking01@gmail.com>
2023-07-09 11:25:29 -0400

    adding menu sound and making footsteps quieter

--- GMTK2023/Assets/Scripts/GameManagement/GameManager.cs
+++ GMTK2023/Assets/Scripts/GameManagement/GameManager.cs
@@ -19,6 +19,7 @@ public class GameManager : MonoBehaviour
     public GameObject pauseButton;
 
     [SerializeField] int startingMoney = 100;
+    [SerializeField] GameObject sceneChangeSound;
 
     private int totalUnitsSpawned;
     private int startingLevelMoney;
@@ -107,6 +108,8 @@ public class GameManager : MonoBehaviour
 
     void OnSceneLoaded(Scene scene, LoadSceneMode mode)
     {
+        GameObject sceneChange = Instantiate(sceneChangeSound, transform.position, transform.rotation);
+        Destroy(sceneChange, .2f);
         StartLevel();
     }
 
